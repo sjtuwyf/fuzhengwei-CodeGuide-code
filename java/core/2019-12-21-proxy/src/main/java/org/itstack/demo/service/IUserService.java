1713a3645a9587135576619a040b398be3aafc1e
wenyf <sjtuwyf@126.com>
2022-01-28 09:39:38 +0800

    Initial commit

---
+++ java/core/2019-12-21-proxy/src/main/java/org/itstack/demo/service/IUserService.java
@@ -0,0 +1,11 @@
+package org.itstack.demo.service;
+
+/**
+ * @author ssqswyf
+ * @date 2022/1/28
+ */
+public interface IUserService {
+
+    String queryUserNameById(String userId);
+
+}
---
+++ java/core/2019-12-21-proxy/src/main/java/org/itstack/demo/service/UserService.java
@@ -0,0 +1,8 @@
+package org.itstack.demo.service;
+
+/**
+ * @author ssqswyf
+ * @date 2022/1/28
+ */
+public class UserService {
+}
